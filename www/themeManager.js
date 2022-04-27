7c96597b56c35735dbaf25aa609d23e00cfdbb48
ghost <ghost@shmoo>
2022-04-27 15:34:39 +0200

    fixed js

--- www/themeManager.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("pressme").onclick = function() { changeTheme("green"); }
-
-function changeTheme(theme) {
-    var element = document.body;
-    element.classList.toggle("dark");
-	console.log("grrrr");
-}
